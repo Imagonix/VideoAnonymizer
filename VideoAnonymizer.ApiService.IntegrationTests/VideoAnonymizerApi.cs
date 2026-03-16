f918c5e4afdf19ac0fe4109b5a661fd1fa280b38
Imagonix <dev@imagonix.com>
2026-03-16 11:15:30 +0100

    implement "I upload a video containing sensitive data"

--- VideoAnonymizer.ApiService.IntegrationTests/VideoAnonymizerApi.cs
+++ VideoAnonymizer.ApiService.IntegrationTests/VideoAnonymizerApi.cs
@@ -1,13 +1,60 @@
+using Aspire.Hosting;
+using Aspire.Hosting.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Text;
+
 namespace VideoAnonymizer.ApiService.IntegrationTests
 {
     [Binding]
     public sealed class VideoAnonymizerApi
     {
-        [Given("I upload a video containing sensitive data")]
-        public void GivenIUploadAVideoContainingSensitiveData()
+        private ScenarioContext _scenarioContext;
+
+        private DistributedApplication App
         {
-            string vodeoPath = "TODO";
-            throw new PendingStepException();
+            get => (DistributedApplication)_scenarioContext[nameof(App)];
+            set => _scenarioContext[nameof(App)] = value;
+        }
+
+        private const string AspireApiServiceKey = "apiservice";
+
+
+        public VideoAnonymizerApi(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        private HttpClient CreteApiServiceHttpClient()
+        {
+                var client = App.CreateHttpClient(AspireApiServiceKey, "https");
+                return client;
+        }
+
+        [BeforeScenario]
+        public async Task SetupEnvironment()
+        {
+            var appHost =
+            await DistributedApplicationTestingBuilder
+                    .CreateAsync<Projects.VideoAnonymizer_AppHost>();
+            appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+            {
+                clientBuilder.AddStandardResilienceHandler();
+            });
+
+            App = await appHost.BuildAsync();
+            await App.StartAsync();
+        }
+
+        [Given("I upload a video containing sensitive data")]
+        public async Task GivenIUploadAVideoContainingSensitiveData()
+        {
+            var currentDirectory = Environment.CurrentDirectory;
+            var vodeoPath = Path.Combine(currentDirectory, "Samples/grok-video-a33c5aac-1cb9-4b9b-bcfb-8dcb1dfcd15e.mp4");
+            var content = new StringContent(vodeoPath, Encoding.UTF8, MediaTypeNames.Text.Plain);
+            using var httpclient = CreteApiServiceHttpClient();
+            await httpclient.PostAsync("/analyze", content);
         }
 
         [When("the video has been analyzed")]
