27994c31a15e067b9d9e1bfd516ed712871d2908
Imagonix <dev@imagonix.com>
2026-03-20 19:44:23 +0100

    avoid circular serialization

--- VideoAnonymizer.Database/AnalyzedFrame.cs
+++ VideoAnonymizer.Database/AnalyzedFrame.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace VideoAnonymizer.Database
 {
@@ -9,6 +10,7 @@ namespace VideoAnonymizer.Database
         public double TimeSeconds { get; set; }
         public virtual ICollection<DetectedObject> DetectedObjects { get; set; }
         public virtual Guid VideoId { get; set; }
+        [JsonIgnore]
         public virtual Video Video { get; set; } = default!;
     }
 }
