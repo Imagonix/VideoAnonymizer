c0e97142bcc6a25d71415703e80da8fe0bb5b00d
Imagonix <dev@imagonix.com>
2026-05-05 13:59:29 +0200

    move action handling back into VideoEditor.razor.cs

--- VideoAnonymizer.Web.Modules/Components/VideoEditor.razor.cs
+++ VideoAnonymizer.Web.Modules/Components/VideoEditor.razor.cs
@@ -33,6 +33,65 @@ public partial class VideoEditor : ComponentBase, IAsyncDisposable
     private int _lastTimeBufferMs;
     private DotNetObjectReference<VideoEditor>? _dotNetRef;
 
+    [JSInvokable]
+    public Task OnDetectedObjectAdded(string videoId, string analyzedFrameId, DetectedObjectDto dto)
+    {
+        return OnAction.InvokeAsync(new ObjectAddedAction
+        {
+            VideoId = videoId,
+            AnalyzedFrameId = analyzedFrameId,
+            Object = dto
+        });
+    }
+
+    [JSInvokable]
+    public Task OnDetectedObjectUpdated(string videoId, string analyzedFrameId, DetectedObjectDto dto, string operationType, DetectedObjectDto[] beforeState)
+    {
+        return OnAction.InvokeAsync(new ObjectUpdatedAction
+        {
+            VideoId = videoId,
+            AnalyzedFrameId = analyzedFrameId,
+            Object = dto,
+            OperationType = operationType,
+            BeforeState = beforeState
+        });
+    }
+
+    [JSInvokable]
+    public Task OnDetectedObjectsBulkUpdated(string videoId, DetectedObjectDto[] dtos, string operationType, DetectedObjectDto[] beforeState)
+    {
+        return OnAction.InvokeAsync(new ObjectsBulkUpdatedAction
+        {
+            VideoId = videoId,
+            Objects = dtos,
+            OperationType = operationType,
+            BeforeState = beforeState
+        });
+    }
+
+    [JSInvokable]
+    public Task OnDetectedObjectDeleted(string videoId, string analyzedFrameId, DetectedObjectDto dto)
+    {
+        return OnAction.InvokeAsync(new ObjectDeletedAction
+        {
+            VideoId = videoId,
+            AnalyzedFrameId = analyzedFrameId,
+            Object = dto
+        });
+    }
+
+    [JSInvokable]
+    public Task OnUndo()
+    {
+        return OnAction.InvokeAsync(new UndoAction());
+    }
+
+    [JSInvokable]
+    public Task OnRedo()
+    {
+        return OnAction.InvokeAsync(new RedoAction());
+    }
+
     public async Task PushChangesToVue(DetectedObjectChangeSet changes)
     {
         if (!_mounted || _hostModule is null) return;
